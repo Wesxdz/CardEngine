b3906484d1a0a9ffb7542d2f0daf6c135558bbe7
Wesley Barlow <sirwesleybarlow@gmail.com>
2018-05-29 11:21:39 -0600

    Fix project type, move all ya stuff to CardApp project

---
+++ CardApp/CardGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardEngine
+{
+    abstract class CardGame
+    {
+        public abstract void Start();
+        public abstract void Exit();
+        public abstract void Save(string path);
+        public abstract void Load(string path);
+    }
+}

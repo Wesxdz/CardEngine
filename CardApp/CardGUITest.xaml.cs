ebb0054f84587297297d7a2db2a12edbe2e14538
Brad Gustafson <greeninja13@gmail.com>
2018-05-29 12:14:56 -0600

    Rectangle that can move

--- CardApp/CardGUITest.xaml.cs
+++ CardApp/CardGUITest.xaml.cs
@@ -19,9 +19,47 @@ namespace CardApp
     /// </summary>
     public partial class CardGUITest : Window
     {
+        private Canvas canvas;
+        private Rectangle card;
+        private Button move;
+        int x = 0, y = 0;
         public CardGUITest()
         {
             InitializeComponent();
+
+            canvas = new Canvas();
+            canvas.Background = new SolidColorBrush(Colors.LightCyan);
+
+            card = new Rectangle();
+            card.Width = 50;
+            card.Height = 75;
+            card.Fill = new SolidColorBrush(Colors.Red);
+
+            Canvas.SetLeft(card, x);
+            Canvas.SetTop(card, y);
+
+            canvas.Children.Add(card);
+
+
+            move = new Button();
+            move.Content = "Move";
+
+            move.Click += Button_Click;
+
+            Canvas.SetLeft(move, 600);
+            Canvas.SetTop(move, 350);
+
+            canvas.Children.Add(move);
+            Content = canvas;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            x += 5;
+            y += 5;
+            Canvas.SetLeft(card, x);
+            Canvas.SetTop(card, y);
+
         }
     }
 }

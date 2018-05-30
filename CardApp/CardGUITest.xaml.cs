e21586f1d305e7c38549ac42474e2272fcbd7801
Lucas <lmckamey@student.neumont.edu>
2018-05-30 11:39:03 -0600

    Menu navigaion | Pages
    Added Individual pages for games and Menu Navigaion works

--- CardApp/CardGUITest.xaml.cs
+++ CardApp/CardGUITest.xaml.cs
@@ -1,65 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Shapes;
 
-namespace CardApp
-{
-    /// <summary>
-    /// Interaction logic for CardGUITest.xaml
-    /// </summary>
-    public partial class CardGUITest : Window
-    {
-        private Canvas canvas;
-        private Rectangle card;
-        private Button move;
-        int x = 0, y = 0;
-        public CardGUITest()
-        {
-            InitializeComponent();
+//namespace CardApp
+//{
+//    /// <summary>
+//    /// Interaction logic for CardGUITest.xaml
+//    /// </summary>
+//    public partial class CardGUITest : Window
+//    {
+//        private Canvas canvas;
+//        private Rectangle card;
+//        private Button move;
+//        int x = 0, y = 0;
+//        public CardGUITest()
+//        {
+//            InitializeComponent();
 
-            canvas = new Canvas();
-            canvas.Background = new SolidColorBrush(Colors.LightCyan);
+//            canvas = new Canvas();
+//            canvas.Background = new SolidColorBrush(Colors.LightCyan);
 
-            card = new Rectangle();
-            card.Width = 50;
-            card.Height = 75;
-            card.Fill = new SolidColorBrush(Colors.Red);
+//            card = new Rectangle();
+//            card.Width = 50;
+//            card.Height = 75;
+//            card.Fill = new SolidColorBrush(Colors.Red);
 
-            Canvas.SetLeft(card, x);
-            Canvas.SetTop(card, y);
+//            Canvas.SetLeft(card, x);
+//            Canvas.SetTop(card, y);
 
-            canvas.Children.Add(card);
+//            canvas.Children.Add(card);
 
 
-            move = new Button();
-            move.Content = "Move";
+//            move = new Button();
+//            move.Content = "Move";
 
-            move.Click += Button_Click;
+//            move.Click += Button_Click;
 
-            Canvas.SetLeft(move, 600);
-            Canvas.SetTop(move, 350);
+//            Canvas.SetLeft(move, 600);
+//            Canvas.SetTop(move, 350);
 
-            canvas.Children.Add(move);
-            Content = canvas;
-        }
+//            canvas.Children.Add(move);
+//            Content = canvas;
+//        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            x += 5;
-            y += 5;
-            Canvas.SetLeft(card, x);
-            Canvas.SetTop(card, y);
+//        private void Button_Click(object sender, RoutedEventArgs e)
+//        {
+//            x += 5;
+//            y += 5;
+//            Canvas.SetLeft(card, x);
+//            Canvas.SetTop(card, y);
 
-        }
-    }
-}
+//        }
+//    }
+//}

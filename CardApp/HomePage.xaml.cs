2defbdd72c59ebb96e48480a474e6466721a398a
JoshuaKey <keyjoshj98@yahoo.com>
2018-06-04 10:33:32 -0600

    Added BlackJackSeup Pagew

--- CardApp/HomePage.xaml.cs
+++ CardApp/HomePage.xaml.cs
@@ -27,7 +27,7 @@ namespace CardApp
 
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
-            Uri page = new Uri("BlackJackPage.xaml", UriKind.Relative);
+            Uri page = new Uri("BlackJackSetupPage.xaml", UriKind.Relative);
             
             NavigationService.Navigate(page); 
         }
